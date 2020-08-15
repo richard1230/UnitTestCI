6049cf044b7e0b5642b8562199534059bdd06806
richard1230 <yongjinhu1230@gmail.com>
2020-08-15 18:25:27 +0800

    DefaultAccountManagerTest version00, it needs changing

--- src/main/java/com/github/JunitInAction/Chapter8/AccountManager/DefaultAccountManager.java
+++ src/main/java/com/github/JunitInAction/Chapter8/AccountManager/DefaultAccountManager.java
@@ -2,22 +2,28 @@ package com.github.JunitInAction.Chapter8.AccountManager;
 
 import com.github.JunitInAction.Chapter8.Account.Account;
 import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 
-import java.util.PropertyResourceBundle;
-import java.util.ResourceBundle;
 
 
 public class DefaultAccountManager implements AccountManager{
 
-    private static final Log logger = LogFactory.getLog(AccountManager.class);
+    private Log logger;
+//    private Configuration configuration;
+
+    public DefaultAccountManager() {
+//        this(LogFactory.getLog(DefaultAccountManager.class),new DefaultConfiguration("technical"));
+    }
+
+    public DefaultAccountManager(Log logger/*, Configuration configuration*/
+    ) {
+        this.logger = logger;
+//        this.configuration = configuration;
+    }
 
     @Override
     public Account findAccountForUser(String userId) {
-        logger.debug("Getting account for user [" + userId + "]");
-        ResourceBundle bundle =
-                PropertyResourceBundle.getBundle("technical");
-        String sql = bundle.getString("FIND_ACCOUNT_FOR_USER");
+        this.logger.debug("Getting account for user [" + userId + "]");
+//        this.configuration.getSQL("FIND_ACCOUNT_FOR_USER");
         return null;
     }
 
---
+++ src/test/java/com/github/JunitInAction/Chapter8/AccountManager/DefaultAccountManagerTest.java
@@ -0,0 +1,19 @@
+package com.github.JunitInAction.Chapter8.AccountManager;
+
+
+import org.junit.jupiter.api.Test;
+
+class DefaultAccountManagerTest {
+
+    @Test
+    public void testFindAccountByUser() {
+//        MockLog logger = new MockLog();
+//        MockConfiguration configuration = new MockConfiguration();
+//        configuration.setSQL("SELECT * [...]");
+//        DefaultAccountManager am = new DefaultAccountManager(logger,
+//                configuration);
+//        Account account = am.findAccountForUser("1234");
+        // Perform asserts here
+//   [...]
+    }
+}
