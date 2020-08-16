8cb19b955af70b7b4570a41cbcd35c2e0d5c221c
richard1230 <yongjinhu1230@gmail.com>
2020-08-16 17:57:31 +0800

    TestAccountServiceEasyMock

--- src/test/java/com/github/JunitInAction/Chapter8/AccountService/AccountServiceTest.java
+++ src/test/java/com/github/JunitInAction/Chapter8/AccountService/AccountServiceTest.java
@@ -3,26 +3,21 @@ package com.github.JunitInAction.Chapter8.AccountService;
 
 import com.github.JunitInAction.Chapter8.Account.Account;
 import com.github.JunitInAction.Chapter8.AccountManager.AccountManager;
-import org.jmock.Expectations;
-import org.jmock.Mockery;
-import org.jmock.junit5.JUnit5Mockery;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.RegisterExtension;
 
+import static org.easymock.EasyMock.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AccountServiceTest {
 
 
-    @RegisterExtension
-    Mockery context = new JUnit5Mockery();
-
     private AccountManager mockAccountManager;
 
     @BeforeEach
     public void setUp() {
-        mockAccountManager = context.mock(AccountManager.class);
+        mockAccountManager = createMock("mockAccountManager", AccountManager.class);
     }
 
     @Test
@@ -30,18 +25,17 @@ class AccountServiceTest {
         Account senderAccount = new Account("1", 200);
         Account beneficiaryAccount = new Account("2", 100);
 
-        context.checking(new Expectations() {
-            {
-                oneOf(mockAccountManager).findAccountForUser("1");
-                will(returnValue(senderAccount));
-                oneOf(mockAccountManager).findAccountForUser("2");
-                will(returnValue(beneficiaryAccount));
+        // Start defining the expectations
+        mockAccountManager.updateAccount(senderAccount);
+        mockAccountManager.updateAccount(beneficiaryAccount);
 
-                oneOf(mockAccountManager).updateAccount(senderAccount);
-                oneOf(mockAccountManager)
-                        .updateAccount(beneficiaryAccount);
-            }
-        });
+        expect(mockAccountManager.findAccountForUser("1"))
+                .andReturn(senderAccount);
+        expect(mockAccountManager.findAccountForUser("2"))
+                .andReturn(beneficiaryAccount);
+
+        // we’re done defining the expectations
+        replay(mockAccountManager);
 
         AccountService accountService = new AccountService();
         accountService.setAccountManager(mockAccountManager);
@@ -49,5 +43,10 @@ class AccountServiceTest {
 
         assertEquals(150, senderAccount.getBalance());
         assertEquals(150, beneficiaryAccount.getBalance());
+    }
+
+    @AfterEach
+    public void tearDown() {
+        verify(mockAccountManager);
     }
 }
--- src/test/java/com/github/JunitInAction/Chapter8/WebClientRefactor/WebClientTest.java
+++ src/test/java/com/github/JunitInAction/Chapter8/WebClientRefactor/WebClientTest.java
@@ -1,23 +1,86 @@
 package com.github.JunitInAction.Chapter8.WebClientRefactor;
 
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.junit5.JUnit5Mockery;
+import org.jmock.lib.legacy.ClassImposteriser;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 
+
+import java.io.IOException;
+import java.io.InputStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class WebClientTest {
+    @RegisterExtension
+    Mockery context = new JUnit5Mockery()
+    {
+        {
+            setImposteriser( ClassImposteriser.INSTANCE );
+        }
+    };
+
     @Test
-    public void testGetContentOk() throws Exception {
-        MockConnectionFactory mockConnectionFactory =
-                new MockConnectionFactory();
+    public void testGetContentOk() throws Exception
+    {
+        ConnectionFactory factory =
+                context.mock( ConnectionFactory.class );
+        InputStream mockStream =
+                context.mock( InputStream.class );
 
-        MockInputStream mockStream = new MockInputStream();
-        mockStream.setBuffer("It works");
-        mockConnectionFactory.setData(mockStream);
+        context.checking( new Expectations()
+        {
+            {
+                oneOf( factory ).getData();
+                will( returnValue( mockStream ) );
+
+                atLeast( 1 ).of( mockStream ).read();
+                will( onConsecutiveCalls(
+                        returnValue( Integer.valueOf ( (byte) 'W' ) ),
+                        returnValue( Integer.valueOf ( (byte) 'o' ) ),
+                        returnValue( Integer.valueOf ( (byte) 'r' ) ),
+                        returnValue( Integer.valueOf ( (byte) 'k' ) ),
+                        returnValue( Integer.valueOf ( (byte) 's' ) ),
+                        returnValue( Integer.valueOf ( (byte) '!' ) ),
+                        returnValue( -1 ) ) );
+
+                oneOf( mockStream ).close();
+            }
+        } );
+
         WebClient client = new WebClient();
-        String workingContent = client.getContent(mockConnectionFactory);
+        String workingContent = client.getContent( factory );
 
-        assertEquals("It works", workingContent);
-        mockStream.verify();
+        assertEquals( "Works!", workingContent );
+    }
+
+    @Test
+    public void testGetContentCannotCloseInputStream() throws Exception
+    {
+
+        ConnectionFactory factory =
+                context.mock( ConnectionFactory.class );
+        InputStream mockStream = context.mock( InputStream.class );
+
+        context.checking( new Expectations()
+        {
+            {
+                oneOf( factory ).getData();
+                will( returnValue( mockStream ) );
+                oneOf( mockStream ).read();
+                will( returnValue( -1 ) );
+                oneOf( mockStream ).close();
+                will( throwException(
+                        new IOException( "cannot close" ) )  );
+            }
+        } );
+
+        WebClient client = new WebClient();
+
+        String workingContent = client.getContent( factory );
+
+        assertNull( workingContent );
     }
 }
